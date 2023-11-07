5f706045c73b5d3d0a6b1b274656f8073ec16176
souzadfs <souzadf09@gmail.com>
2023-11-06 22:27:14 -0300

    Change name, from python to alistamento

---
+++ comparandon°.py
@@ -0,0 +1,9 @@
+num_1 = float(input('Digite o primeiro numero: '))
+num_2 = float(input('Digite o segundo numero: '))
+
+if num_1 > num_2:
+    print(f' O primeiro {num_1} é maior que segundo numero {num_2}')
+elif num_2 > num_1:
+    print(f'O segundo numero {num_2} é maior que o primeiro. ')
+else:
+    print(f'O primeiro numero {num_1} e o segundo numero {num_2} são iguais.') 
