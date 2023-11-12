0f3be4b2ba889a51f3c4bce85e538d91bf20af3d
souzadfs <souzadf09@gmail.com>
2023-11-12 13:25:17 -0300

    athelete classification

--- Python_if_Elif_Else/classificado_atetlas.py
+++ Python_if_Elif_Else/classificado_atetlas.py
@@ -4,14 +4,16 @@ idade_atleta = int(input("Qual é o seu ano de nascimento? "))
 ano_atual = date.today().year
 idade_atual = ano_atual - idade_atleta
 
-if 16 <= idade_atual <= 18:
+if 12 <= idade_atual <= 15:
+    print(f"sua idade é {idade_atual} anos, e você está na categoria MIRIM ! ")   
+elif 16 < idade_atual <= 20:
     print(f" sua idade é {idade_atual} anos, e você está na categoria JUNIOR !!")
-elif 18 < idade_atual <= 29:
+elif 20 < idade_atual <= 29:
     print(f"Sua idade é {idade_atual} anos, e sua categoria é PLENO !!")
-elif 29 < idade_atual <= 50:
+elif 29 < idade_atual <= 40:
     print(f"Sua idade é {idade_atual} anos, e sua categoria é SENIOR !!")
-elif idade_atual > 50:
-    print(f"Sua idade é {idade_atual} anos, e você já esta APOSENTADO !!")    
+elif idade_atual > 40:
+    print(f"Sua idade é {idade_atual} anos, e você já esta MASTER !!")    
 else:
     print(f"Sua idade é {idade_atual}, e você não tem categoria.")    
 
