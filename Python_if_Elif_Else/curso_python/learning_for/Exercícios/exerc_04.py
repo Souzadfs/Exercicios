d2c375cdaf2a52d97586682fd259f986bbab553f
souzadfs <souzadf09@gmail.com>
2023-11-27 21:03:59 -0300

    exercicos

---
+++ Python_if_Elif_Else/curso_python/learning_for/Exercícios/exerc_04.py
@@ -0,0 +1,9 @@
+soma = 0
+
+for cont in range(1, 501, 2):
+    if cont %3 == 0:
+        print(cont, end= ' - ')
+        soma = soma + cont
+print( soma, end= ' - ')
+   #print(cont, end= ' - ')
+        
