b8c33984489ac177fce805093eac4f13b8cb9608
souzadfs <souzadf09@gmail.com>
2023-12-28 21:21:49 -0300

    commit

--- Python_if_Elif_Else/curso_python/learning_for/Exercícios_for/exerc_07.py
+++ Python_if_Elif_Else/curso_python/learning_for/Exercícios_for/exerc_07.py
@@ -1,11 +1,14 @@
 frase = str(input('Digite uma frase: ')).strip(). upper()
 palavra = frase.split()
-juntar = ''.join((palavra))
+juntar = '*'.join((palavra))
 inverter = ''
 
 for letras in range(len(juntar) - 1, -1, -1):
     inverter += juntar[letras]
 if inverter == juntar:
-    print(f' A palavra digita {juntar} é um PALINDROMO')
+    print(f'{juntar}')
+    print(f' A palavra digita, {inverter} é um PALINDROMO')
 else:
-    print(f' A palavra digita {juntar} não é um PALINDROMO')  
+    print(f'{juntar}')
+    print(f' A palavra digita invertida {inverter}, não é um PALINDROMO')  
+
